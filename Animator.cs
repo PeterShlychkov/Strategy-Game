da8e1eece86f613986fac0abf48d82360fe15bf2
Peter Shlychkov <42078422+PeterShlychkov@users.noreply.github.com>
2024-01-01 19:44:38 -0800

    Update and rename animator.cs to Animator.cs

--- Animator.cs
+++ Animator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animator : MonoBehaviour
+public class Animator : MonoBehaviour
 {
     public int speed = 60;
 
